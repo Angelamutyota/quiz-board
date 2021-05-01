8640ea8eee90a70fefe696da733a609478c43179
ANGELA <angela.mbithe@student.moringaschool.com>
2021-05-01 21:11:02 +0300

    add js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    ("#submit").click(function (event) {
+        event.preventDefault()
+          let quiz1 = $("#quiz1 option:selected").val();
+          let quiz2 = $("#quiz2 option:selected").val();
+          let quiz3 = $("#quiz3 option:selected").val();
+          let quiz4 = $("#quiz4 option:selected").val();
+          let quiz5 = $("#quiz5 option:selected").val();
+          let quiz6 = $("#quiz6 option:selected").val();
+          let quiz7 = $("#quiz7 option:selected").val();
+          let quiz8 = $("#quiz8 option:selected").val();
+          let quiz9 = $("#quiz9 option:selected").val();
+          let quiz10 = $("#quiz10 option:selected").val();
+
+    })
+})
