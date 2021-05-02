a11340841408c309692704bc4d7b347dd7afd261
ANGELA <angela.mbithe@student.moringaschool.com>
2021-05-02 15:22:18 +0300

    add js functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
 
-    ("#submit").click(function (event) {
-        event.preventDefault()
-          let quiz1 = $("#quiz1 option:selected").val();
-          let quiz2 = $("#quiz2 option:selected").val();
-          let quiz3 = $("#quiz3 option:selected").val();
-          let quiz4 = $("#quiz4 option:selected").val();
-          let quiz5 = $("#quiz5 option:selected").val();
-          let quiz6 = $("#quiz6 option:selected").val();
-          let quiz7 = $("#quiz7 option:selected").val();
-          let quiz8 = $("#quiz8 option:selected").val();
-          let quiz9 = $("#quiz9 option:selected").val();
-          let quiz10 = $("#quiz10 option:selected").val();
+        
+     $("#submit").click(function(event){
+        event.preventDefault();
+        var result = 0;
+           $("input[type='radio']:checked").each(function(){
+             result += parseInt($(this).val());
+        });
+           $("#total").html(result);
+    });
 
-    })
 })
